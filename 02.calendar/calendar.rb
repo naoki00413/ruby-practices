3b9844ef48dfe032f82ac76d6fb2e79d62e5f407
naoki00413 <popoteto0413@shiolab.com>
2023-02-16 08:12:26 +0900

    カレンダープログラムの指摘点を修正
    修正点 不要なコメントを削除, 出力のコードを少し変更
    本家カレンダーと表記が合うように修正, カンマにスペースを追加
    崩れてたインデントを修正, 改行の出力を違和感ないように修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,23 +1,22 @@
 require "date"
 require 'optparse'
-# 年月の初期設定
+
 params = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
 year = params.values[0].to_i
 month = params.values[1].to_i
-# 年月の出力
-year_month = month.to_s + "月" .center(2)+ year.to_s
-# 出力して年月を中央寄せにする
-puts year_month.center(18)
-# 曜日の出力
+
+year_month = "#{month}月 #{year}"
+puts year_month.center(20)
+
 puts "日 月 火 水 木 金 土"
-# 日付の出力
-first_wday = Date.new(year,month,1).wday
-last_day = Date.new(year, month,-1).day
+
+first_wday = Date.new(year, month, 1).wday
+last_day = Date.new(year, month, -1).day
 print "   " * first_wday
 
 (1..last_day).each { |day|
-    printf("%2d ", day)
-     if (first_wday + day) % 7 == 0 || day == last_day
-      puts "\n"
-     end
-    }
+  printf("%2d ", day)
+    if (first_wday + day) % 7 == 0 || day == last_day
+    print "\n"
+    end
+}
