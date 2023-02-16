987626b94fa24358c221464b2261684f5766e3b0
naoki00413 <popoteto0413@shiolab.com>
2023-02-16 13:51:00 +0900

    カレンダープログラム 指摘点のブロックを{}を変更
    修正点：指摘点の{}からdo/endに修正、19-21行目のインデントを再修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -10,13 +10,13 @@ puts year_month.center(20)
 
 puts "日 月 火 水 木 金 土"
 
-first_wday = Date.new(year, month, 1).wday
-last_day = Date.new(year, month, -1).day
-print "   " * first_wday
+first_date = Date.new(year, month, 1).wday
+last_date = Date.new(year, month, -1).day
+print "   " * first_date
 
-(1..last_day).each { |day|
-  printf("%2d ", day)
-    if (first_wday + day) % 7 == 0 || day == last_day
+(1..last_date).each do |wday|
+  printf("%2d ", wday)
+  if (first_date + wday) % 7 == 0 || wday == last_date
     print "\n"
-    end
-}
+  end
+end
