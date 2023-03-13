3c1580277954b272ae8682a010ce4aed9fe49e68
naoki00413 <popoteto0413@shiolab.com>
2023-03-13 10:23:10 +0900

    指摘点を修正
    ljustの数値を10から20に変更 最大３列表示できるように変更 使ってないメソッドの削除

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,17 +2,12 @@
 
 require 'optparse'
 
-def option_process
-  optparse = ARGV.getopts('', 'ls:')
-  puts optparse
-end
-
 def file_data_list
   lsfile_date = Dir.glob('*')
-  filename = lsfile_date.each_slice(2).to_a
+  filename = lsfile_date.each_slice(3).to_a
   file_sort = filename[0].zip(*filename[1..])
   file_sort.each do |linefeed|
-    linefeed.each { print _1.ljust(10) }
+    linefeed.each { print _1.ljust(20) }
     puts
   end
 end
