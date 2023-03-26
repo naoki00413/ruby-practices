fbdeddc3bb4887183d9914c35b7303b65a8313dd
naoki00413 <popoteto0413@shiolab.com>
2023-03-27 07:06:27 +0900

    変数名の変更をしました
    変数名をfile_replaceからtransposed_filesに変更しました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,9 +5,9 @@ def file_list
   quotient, remainder = file_names.count.divmod(3)
   chunk_size = remainder.zero? ? quotient : quotient + 1
   chunked_files = file_names.each_slice(chunk_size).to_a
-  file_replace = chunked_files[0].zip(*chunked_files[1..]) # ファイルの並び替え
+  transposed_files = chunked_files[0].zip(*chunked_files[1..]) # ファイルの並び替え
   get_max_length_file = file_names.max_by(&:length) # 最大文字数名のファイルを取得
-  file_replace.each do |reorder|
+  transposed_files.each do |reorder|
     reorder.compact.each { print _1.ljust(get_max_length_file.length + 3) }
     puts # ファイルの最大文字数＋３で出力
   end
